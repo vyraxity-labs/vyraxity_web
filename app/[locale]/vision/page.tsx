02f66c3eae3c17118aa48310a0df38cbd2d27d95
OlaOluwalekan <olalekanbello534@gmail.com>
2026-09-11 14:07:43 +0100

    feat: implement vision page and add corresponding i18n localization content

--- app/[locale]/vision/page.tsx
+++ app/[locale]/vision/page.tsx
@@ -1,9 +1,12 @@
 import { VisionHero } from "@/components/sections/vision/VisionHero";
+import { OurAmbition } from "@/components/sections/vision/OurAmbition";
 
 export default function VisionPage() {
   return (
     <>
       <VisionHero />
+      <OurAmbition />
     </>
   );
 }
+
