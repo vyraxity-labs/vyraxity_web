4f5f2b0abb328f8f14260a025f6a6900f237583b
OlaOluwalekan <olalekanbello534@gmail.com>
2026-09-12 11:02:35 +0100

    feat: add contact page with FollowTheJourney component and English localization

--- app/[locale]/contact/page.tsx
+++ app/[locale]/contact/page.tsx
@@ -1,12 +1,15 @@
 import { ContactHero } from '@/components/sections/contact/ContactHero'
 import { ContactForm } from '@/components/sections/contact/ContactForm'
+import { FollowTheJourney } from '@/components/sections/contact/FollowTheJourney'
 
 export default function ContactPage() {
   return (
     <>
       <ContactHero />
       <ContactForm />
+      <FollowTheJourney />
     </>
   )
 }
+
 
